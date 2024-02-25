0ad555261007c199326e7611a400aa62ef1c0059
davib <davi.brantes@gmail.com>
2024-02-24 21:12:45 -0300

    feat: add queries de consulta por id e especialidade

--- src/main/java/medisync/api/domain/doctor/DoctorRepository.java
+++ src/main/java/medisync/api/domain/doctor/DoctorRepository.java
@@ -3,7 +3,37 @@ package medisync.api.domain.doctor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     Page<Doctor> findAllByActiveTrue(Pageable pageable);
+
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
+                and
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+        """)
+    Doctor chooseRandomDoctor(Specialty specialty, LocalDateTime date);
+
+    @Query("""
+            select d.active
+            from Doctor m
+            where
+            d.id = :id
+            """)
+    Boolean findActiveById(Long id);
 }
